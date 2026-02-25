913e3c2a2ec6641126f41ecec3fcfc0662686ff7
KevOneRedOne <kevinalves@hotmail.fr>
2026-02-25 23:01:20 +0100

    feat: add app version link in footer with dynamic version display and GitHub releases URL

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,14 @@
  */
 
 import { TetrisGame } from '@/app/TetrisGame';
+import { APP_VERSION, REPO_RELEASES_URL } from '@constants/config';
 import './styles/main.scss';
 
 document.addEventListener('DOMContentLoaded', () => {
+  const versionLink = document.getElementById('app-version-link');
+  if (versionLink && versionLink instanceof HTMLAnchorElement) {
+    versionLink.textContent = `v${APP_VERSION}`;
+    versionLink.href = `${REPO_RELEASES_URL}/tag/v${APP_VERSION}`;
+  }
   new TetrisGame();
 });
